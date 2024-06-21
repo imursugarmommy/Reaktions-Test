c087adde8f18f5370ed6c5f8af9e3a2bc0bb7346
Levi <139384471+imursugarmommy@users.noreply.github.com>
2024-06-21 13:48:23 +0200

    redirecting issue fixed
    hopefully

--- docs/Statistics/stats.js
+++ docs/Statistics/stats.js
@@ -23,7 +23,7 @@ const maxWeeks = 4;
 document.addEventListener("DOMContentLoaded", async () => {
   document
     .querySelector(".back-to-leaderboard")
-    .addEventListener("click", () => (location.href = "../leaderboard.html"));
+    .addEventListener("click", () => (location.href = "/leaderboard.html"));
 
   const userID = userCreds.uid;
   const userDisplay = document.querySelector(".username-display");
